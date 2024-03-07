0eb38f2d7e035e5993d0b7f1f01a51f5a35f0155
Pelzang123 <you@example.com>
2024-03-07 14:15:16 +0600

    Check for positive and negative number

---
+++ positivenegative.py
@@ -0,0 +1,9 @@
+num = float(input("Please type any number:"))
+if num < 0:
+    print("The number is negative ")
+elif num > 0:
+    print("The number is positive ")
+
+elif num == 0:
+    print("The number is zero ")
+    
